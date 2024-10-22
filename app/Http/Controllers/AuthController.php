c7f02f5452d68ff729ad590cf941b5568e0e413a
Ylber Prapashtica <ylberprapashtica@gmail.com>
2024-10-22 11:55:00 +0200

    Add password reset functionality.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,12 +7,12 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
 class AuthController extends Controller
 {
-
-    /**
-     * Handle an authentication attempt.
-     */
     public function login(Request $request): JsonResponse
     {
         $credentials = $request->validate([
@@ -20,9 +20,9 @@ class AuthController extends Controller
             'password' => ['required'],
         ]);
 
+        //I need not login a user when another one is already logged in
         if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
-
             return new ApiResponse('User has been logged in.');
         }else {
             return new ApiResponse('The provided credentials do not match our records.');
@@ -40,5 +40,63 @@ class AuthController extends Controller
         $userEntity = User::create($userData);
 
         return new ApiResponse('New user is stored', $userEntity);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return new ApiResponse('User is now logged out.');
+    }
+
+    // Send Password Reset Link
+    public function sendResetLinkEmail(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+        ]);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        if ($status == Password::RESET_LINK_SENT) {
+            return response()->json(['message' => __($status)]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => [trans($status)],
+        ]);
+    }
+
+    // Reset Password
+    public function reset(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password),
+                ])->save();
+            }
+        );
+
+        if ($status == Password::PASSWORD_RESET) {
+            return response()->json(['message' => __($status)]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => [trans($status)],
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => ['web']], function () {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
-});
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::post('password/email', [AuthController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::post('password/reset', [AuthController::class, 'reset'])->name('password.reset');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
